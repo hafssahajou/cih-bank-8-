88fdd46d6060394ee039a9fb369e664413d4eafb
hafssahajou <hafssahajou200@gmail.com>
2023-12-19 16:03:01 +0100

    changes

--- app/views/login.php
+++ app/views/login.php
@@ -50,20 +50,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <section class="vh-100">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-xl-10">
+                <div class="col col-xl-5">
                     <div class="card" style="border-radius: 1rem;">
-                        <div class="row g-0">
-                            <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="https://i.pinimg.com/564x/10/c7/56/10c756e166be6af77a89ed4fd8b71b5e.jpg" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height:100%;" />
-                            </div>
-                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                        <!-- <div class="row g-0"> -->
+                            <!-- <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="IMG/pexels-photo-6781340.jpeg" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height:100%;" />
+                            </div> -->
+                            <div class="col-md-6 col-lg-7 w-100 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
                                     
                                     <form action="" method="post">
                                         
                                     <div class="d-flex align-items-center mb-3 pb-1">
                                         <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                                        <span class="h1 fw-bold mb-0">Logo</span>
+                                        <span class="h1 fw-bold mb-0">HEY</span>
                                     </div>
                                     
                                     <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
@@ -79,14 +79,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                                     </div>
                                     
                                     <div class="pt-1 mb-4">
-                    <button class="btn btn-dark btn-lg btn-block">Login</button>
+                                    <button class="btn btn-dark btn-lg btn-block btn-red">Login</button>
+
                 </div>
                 
-                <a class="small text-muted" href="#!">Forgot password?</a>
+                <!-- <a class="small text-muted" href="#!">Forgot password?</a>
                 <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
                 style="color: #393f81;">Register here</a></p>
                 <a href="#!" class="small text-muted">Terms of use.</a>
-                <a href="#!" class="small text-muted">Privacy policy</a>
+                <a href="#!" class="small text-muted">Privacy policy</a> -->
             </form>
             
         </div>
